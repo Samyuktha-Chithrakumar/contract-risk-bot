87e9f9542666ce46bd0853cd4c43b7b6be67bee2
Samyuktha-Chithrakumar <samyukthachithrakumar@gmail.com>
2026-02-06 15:53:14 +0530

    Auto-download spaCy model for Streamlit Cloud

--- nlp/ner.py
+++ nlp/ner.py
@@ -1,14 +1,26 @@
 import spacy
-nlp = spacy.load("en_core_web_sm")
+from spacy.cli import download
+
+def load_spacy_model():
+    try:
+        return spacy.load("en_core_web_sm")
+    except OSError:
+        download("en_core_web_sm")
+        return spacy.load("en_core_web_sm")
+
+nlp = load_spacy_model()
+
 
 def extract_entities(text):
     doc = nlp(text)
+
     entities = {
         "Parties": [],
         "Dates": [],
         "Money": [],
         "Locations": []
     }
+
     for ent in doc.ents:
         if ent.label_ == "ORG":
             entities["Parties"].append(ent.text)
@@ -18,4 +30,5 @@ def extract_entities(text):
             entities["Money"].append(ent.text)
         elif ent.label_ in ["GPE", "LOC"]:
             entities["Locations"].append(ent.text)
+
     return entities
